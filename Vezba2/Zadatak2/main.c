161b0b5959d773df8eb6f8e7c88662ae9c9efa0a
anja-tanovic <tanovicanja@gmail.com>
2021-05-04 16:31:44 +0200

    pwm triangle

--- Vezba2/Zadatak2/main.c
+++ Vezba2/Zadatak2/main.c
@@ -3,6 +3,7 @@
 
 volatile unsigned char fi = 0;  //faktor ispune u pocetnom trenutku
 volatile unsigned long pr = 0;  //povecava se svaki put kad se pojavi prekid (posle 781 se uvecava fi)
+volatile unsigned char smer = 0;//0 za rast, 1 za opadanje
 
 ISR(TIMER0_COMPA_vect)
 {
@@ -10,8 +11,20 @@ ISR(TIMER0_COMPA_vect)
 
 	if (pr == 781)
 	{
-		fi++;
 		pr = 0;
+		switch (smer){
+			case 0:
+				fi++;
+				if (fi == 255)
+					smer = 1;
+				break;
+			case 1:
+				fi--;
+				if (fi == 0)
+					smer = 0;
+				break;
+		}
+
 	}
 
 	if (pr == 0){
